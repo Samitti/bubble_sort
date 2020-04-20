2ade273b037ec8a1e7aa23d5b003563af14fe656
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-04-20 15:43:25 +0600

    Revise several linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,27 +1,27 @@
 def bubble_sort(arr)
-    i = 0
-    while i < arr.length - 1
-      if arr[i] > arr[i + 1]
-        arr[i], arr[i + 1] = arr[i + 1], arr[i]
-        i = 0
-      else
-        i += 1
-      end
+  i = 0
+  while i < arr.length - 1
+    if arr[i] > arr[i + 1]
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      i = 0
+    else
+      i += 1
     end
-    arr
   end
-  puts bubble_sort([4, 3, 78, 2, 0, 2])
+  arr
+end
+puts bubble_sort([4, 3, 78, 2, 0, 2])
 
-  def sort_bubble(array)
-    count = 0
-    array.each do
-        for num in(0...array.length-1)
-          if yield(array[num], array[num+1]).positive?
-            array[num], array[num+1] = array[num+1],array[num]              
-          end
-        end     
-    end
-   array
+def sort_bubble(array)
+  array.each do
+     (array.length - 1).times do |num|
+        if yield(array[num], array[num + 1]).positive?
+        array[num], array[num + 1] = array[num + 1], array[num]
+        end
+      end
+   end
+  array
   end
-  puts sort_bubble(["hello", "hi", "hey"]){|right, left| right.length - left.length}
+  puts sort_bubble (%w[hello hi hey]) {|right, left| right.length - left.length}
+
   
